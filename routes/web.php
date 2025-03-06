ac8ac88d6a42e03f94480577eedf5ddd1ef869aa
alfnsn <alfiansantika@gmail.com>
2025-03-06 12:11:18 +0700

    login dan database lagi

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/mahasiswa/dashboard', function () {
-        return view('mahasiswa.dashboard');
-    })->name('mahasiswa.dashboard');
+    Route::get('/Mahasiswa/dashboard', function () {
+        return view('Mahasiswa.dashboard');
+    })->name('Mahasiswa.dashboard');
 
     Route::get('/kaprodi/dashboard', function () {
         return view('kaprodi.dashboard');
